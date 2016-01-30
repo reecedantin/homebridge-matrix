bd2a14d8611d94ecbd892d5785a0609331a071e9
Thomas Dantin JR <reecedantin@THOMAS-MAC.local>
2016-01-30 17:41:42 -0500

    initial commit

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
 
-  homebridge.registerAccessory("homebridge-tcp", "TCP", TcpAccessory);
+  homebridge.registerAccessory("homebridge-matrix", "HDMIMatrix", MatrixAccessory);
 }
 
-function TcpAccessory(log, config) {
+function MatrixAccessory(log, config) {
   this.log = log;
   this.service = 'Switch';
 
@@ -25,7 +25,7 @@ function TcpAccessory(log, config) {
   this.port          = config['port'];
 }
 
-TcpAccessory.prototype.matchesString = function(match) {
+MatrixAccessory.prototype.matchesString = function(match) {
   if(this.exactMatch) {
     return (match === this.onValue);
   }
@@ -34,32 +34,50 @@ TcpAccessory.prototype.matchesString = function(match) {
   }
 }
 
-TcpAccessory.prototype.setState = function(powerOn, callback) {
+MatrixAccessory.prototype.setState = function(powerOn, callback) {
   var accessory = this;
   var state = powerOn ? 'on' : 'off';
   var prop = state + 'Command';
   var command = accessory[prop];
   var host = this.host;
   var port = this.port;
+  var pass = 0;
 
   accessory.log('starting set');
 
   var client = new net.Socket();
   client.connect(port, host, function() {
       accessory.log('CONNECTED TO: ' + host + ':' + port);
-      var date = new Date();
-      var curDate = null;
-      do { curDate = new Date(); }
-      while(curDate-date < 1000);
-      accessory.log('Was it written? -' + client.write(command + '\n') + '- '+ command);
-    });
-
-
+  });
 
   client.on('data', function(data) {
       accessory.log('DATA: ' + data.toString('utf-8').trim());
-      // Close the client socket completely
-      client.destroy();
+      var state = data.toString('utf-8').trim();
+      var checkready = state.split("\n");
+      var isCarrot = checkready[checkready.length-1];
+      accessory.log("is this a carrott? -"+isCarrot);
+      if(pass == 0)
+      {
+        accessory.log('Was it written? -admin');
+        client.write('admin\r\n');
+      }
+      else if(pass == 1)
+      {
+        client.write('123\r\n');
+        accessory.log('Was it written? -123');
+      }
+      else if(pass == 2)
+      {
+        client.write(command +'\r\n');
+        accessory.log('Was it written? -' + command);
+      }
+      else if(pass > 2 && isCarrot == ">")
+      {
+        accessory.log("is this a carrott? -"+isCarrot);
+        client.destroy();
+        pass = -1;
+      }
+      pass++;
   });
 
   client.on('close', function() {
@@ -73,48 +91,70 @@ TcpAccessory.prototype.setState = function(powerOn, callback) {
   });
 }
 
-TcpAccessory.prototype.getState = function(callback) {
+MatrixAccessory.prototype.getState = function(callback) {
   var accessory = this;
-  var command = accessory['stateCommand'];
+  var command = 'STMAP';
   var host = this.host;
   var port = this.port;
+  var i = true
+
+  accessory.log('starting get');
 
   var client = new net.Socket();
   client.connect(port, host, function() {
       accessory.log('CONNECTED TO: ' + host + ':' + port);
-      var date = new Date();
-      var curDate = null;
-      do { curDate = new Date(); }
-      while(curDate-date < 1000);
-      accessory.log('Was it written? -' + client.write(command + '\n') + '- '+ command);
   });
 
   client.on('data', function(data) {
       accessory.log('DATA: ' + data.toString('utf-8').trim());
       var state = data.toString('utf-8').trim();
-      accessory.log('State of ' + accessory.name + ' is: ' + state);
-      callback(null, accessory.matchesString(state));
-      client.destroy();
+
+      if(pass == 0)
+      {
+        accessory.log('Was it written? -admin');
+        client.write('admin\r\n');
+        pass++;
+      }
+      else if(pass == 1)
+      {
+        client.write('123\r\n');
+        accessory.log('Was it written? -123');
+
+        pass++;
+      }
+      else if(pass == 2)
+      {
+        client.write(command +'\r\n');
+        accessory.log('Was it written? -' + command);
+        pass++;
+      }
+      else if(pass == 3)
+      {
+        client.destroy();
+        pass = 0;
+      }
   });
 
-  accessory.log('here now');
+  client.on('close', function() {
+    accessory.log('Set ' + accessory.name + ' to ' + state);
+    callback(null);
+  });
 
   client.on('error', function (err) {
     accessory.log('Error: ' + err);
-    callback(err || new Error('Error getting state from ' + accessory.name));
+    callback(err || new Error('Error setting ' + accessory.name + ' to ' + state));
   });
 
-  accessory.log('all done');
 }
 
-TcpAccessory.prototype.getServices = function() {
+MatrixAccessory.prototype.getServices = function() {
   var informationService = new Service.AccessoryInformation();
   var switchService = new Service.Switch(this.name);
 
   informationService
-  .setCharacteristic(Characteristic.Manufacturer, 'TCP Manufacturer')
-  .setCharacteristic(Characteristic.Model, 'TCP Model')
-  .setCharacteristic(Characteristic.SerialNumber, 'TCP Serial Number');
+  .setCharacteristic(Characteristic.Manufacturer, 'Matrix Manufacturer')
+  .setCharacteristic(Characteristic.Model, 'Matrix Model')
+  .setCharacteristic(Characteristic.SerialNumber, 'Matrix Serial Number');
 
   var characteristic = switchService.getCharacteristic(Characteristic.On)
   .on('set', this.setState.bind(this));
