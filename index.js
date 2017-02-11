33e2ee731ac1250278c2ad94c98661140066b83c
reecedantin <reecedantin@yahoo.com>
2017-02-10 22:01:40 -0500

    first commit

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
 
-  homebridge.registerAccessory("homebridge-matrix", "HDMIMatrix", MatrixAccessory);
+  homebridge.registerAccessory("homebridge-sharptv", "SharpTV", SharpTVAccessory);
 }
 
-function MatrixAccessory(log, config) {
+function SharpTVAccessory(log, config) {
   this.log = log;
   this.service = 'Switch';
 
@@ -18,14 +18,14 @@ function MatrixAccessory(log, config) {
   this.onCommand	   = config['on'];
   this.offCommand	   = config['off'];
   this.stateCommand	 = config['state'];
-  this.onValue		   = config['on_value'] || "playing";
+  this.onValue		   = config['on_value'];
   this.onValue	   	 = this.onValue.trim();
   this.exactMatch	   = config['exact_match'] || true;
   this.host          = config['host'];
   this.port          = config['port'];
 }
 
-MatrixAccessory.prototype.matchesString = function(match) {
+SharpTVAccessory.prototype.matchesString = function(match) {
   if(this.exactMatch) {
     return (match === this.onValue);
   }
@@ -34,50 +34,28 @@ MatrixAccessory.prototype.matchesString = function(match) {
   }
 }
 
-MatrixAccessory.prototype.setState = function(powerOn, callback) {
+SharpTVAccessory.prototype.setState = function(powerOn, callback) {
   var accessory = this;
   var state = powerOn ? 'on' : 'off';
   var prop = state + 'Command';
   var command = accessory[prop];
   var host = this.host;
   var port = this.port;
-  var pass = 0;
 
   accessory.log('starting set');
 
   var client = new net.Socket();
+  client.setTimeout(5000, function(err){
+      accessory.log('Timed out connecting to ' + host + ':' + port);
+
+  });
   client.connect(port, host, function() {
       accessory.log('CONNECTED TO: ' + host + ':' + port);
+      pass = 0;
   });
 
   client.on('data', function(data) {
-      accessory.log('DATA: ' + data.toString('utf-8').trim());
-      var state = data.toString('utf-8').trim();
-      var checkready = state.split("\n");
-      var isCarrot = checkready[checkready.length-1];
-      accessory.log("is this a carrott? -"+isCarrot);
-      if(pass == 0)
-      {
-        accessory.log('Was it written? -admin');
-        client.write('admin\r\n');
-      }
-      else if(pass == 1)
-      {
-        client.write('123\r\n');
-        accessory.log('Was it written? -123');
-      }
-      else if(pass == 2)
-      {
-        client.write(command +'\r\n');
-        accessory.log('Was it written? -' + command);
-      }
-      else if(pass > 2 && isCarrot == ">")
-      {
-        accessory.log("is this a carrott? -"+isCarrot);
-        client.write('q\r\n');
-        pass = -1;
-      }
-      pass++;
+      client.write(command + '\r\n');
   });
 
   client.on('close', function() {
@@ -87,74 +65,61 @@ MatrixAccessory.prototype.setState = function(powerOn, callback) {
 
   client.on('error', function (err) {
     accessory.log('Error: ' + err);
+    client.destroy();
     callback(err || new Error('Error setting ' + accessory.name + ' to ' + state));
+  });
+
+  client.on('timeout', function () {
+    client.destroy();
+    callback(new Error('Error setting ' + accessory.name + ' to ' + state + ' -timedout'));
   });
 }
 
-MatrixAccessory.prototype.getState = function(callback) {
+SharpTVAccessory.prototype.getState = function(callback) {
   var accessory = this;
-  var command = 'STMAP';
+  var command = accessory['stateCommand'];
   var host = this.host;
   var port = this.port;
-  var i = true
-
-  accessory.log('starting get');
+  var state;
 
   var client = new net.Socket();
+  client.setTimeout(5000, function(err){
+      accessory.log('Timed out connecting to ' + host + ':' + port);
+
+  });
   client.connect(port, host, function() {
-      accessory.log('CONNECTED TO: ' + host + ':' + port);
+      client.write(command + '\r\n');
   });
 
   client.on('data', function(data) {
-      accessory.log('DATA: ' + data.toString('utf-8').trim());
-      var state = data.toString('utf-8').trim();
-
-      if(pass == 0)
-      {
-        accessory.log('Was it written? -admin');
-        client.write('admin\r\n');
-        pass++;
-      }
-      else if(pass == 1)
-      {
-        client.write('123\r\n');
-        accessory.log('Was it written? -123');
-
-        pass++;
-      }
-      else if(pass == 2)
-      {
-        client.write(command +'\r\n');
-        accessory.log('Was it written? -' + command);
-        pass++;
-      }
-      else if(pass == 3)
-      {
-        client.destroy();
-        pass = 0;
-      }
+      state = data.toString('utf-8').trim();
+      client.end();
   });
 
   client.on('close', function() {
-    accessory.log('Set ' + accessory.name + ' to ' + state);
-    callback(null);
+    accessory.log('State of ' + accessory.name + ' is: ' + state);
+    callback(null, accessory.matchesString(state));
   });
 
   client.on('error', function (err) {
     accessory.log('Error: ' + err);
-    callback(err || new Error('Error setting ' + accessory.name + ' to ' + state));
+    client.destroy();
   });
 
+  client.on('timeout', function () {
+    client.destroy();
+    callback(new Error('Error setting ' + accessory.name + ' to ' + state + ' -timedout'));
+  });
 }
 
-MatrixAccessory.prototype.getServices = function() {
+SharpTVAccessory.prototype.getServices = function() {
   var informationService = new Service.AccessoryInformation();
   var switchService = new Service.Switch(this.name);
 
   informationService
-  .setCharacteristic(Characteristic.Manufacturer, 'Matrix Manufacturer')
-  .setCharacteristic(Characteristic.Model, 'Matrix Model')
-  .setCharacteristic(Characteristic.SerialNumber, 'Matrix Serial Number');
+  .setCharacteristic(Characteristic.Manufacturer, 'SharpTV Manufacturer')
+  .setCharacteristic(Characteristic.Model, 'SharpTV Model')
+  .setCharacteristic(Characteristic.SerialNumber, 'SharpTV Serial Number');
 
   var characteristic = switchService.getCharacteristic(Characteristic.On)
   .on('set', this.setState.bind(this));
